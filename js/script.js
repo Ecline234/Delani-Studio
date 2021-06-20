d840966beb938ed9f114bd2ed7475c77c98b4dab
Ecline Koech <eclinejerutokoech@gmail.com>
2021-06-20 08:19:14 +0300

    modified js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $(".design-hidden").toggle();
+        $(".one").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#dev").click(function(){
+        $(".dev-hidden").toggle();
+        $("#two").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#project").click(function(){
+        $(".project-hidden").toggle();
+        $("#three").toggle();
+    });
+});
